c980e44bc7caee3e5e5b4d8abc89d7c97bd65e23
emirsahovic <emir.sahovic00@gmail.com>
2022-02-08 22:49:35 +0100

    Simple navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-700 shadow-xl w-full p-4 mb-16">
+            <div className="container mx-auto">
+                <h3 className="text-green-500 text-2xl font-bold">Breaking Bad App</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
