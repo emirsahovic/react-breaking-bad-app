90ea2badc34b2d4e9e05718c6c88c3e118ef1b4c
emirsahovic <emir.sahovic00@gmail.com>
2022-02-09 11:51:53 +0100

    Added logo to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
+import logo from '../../images/logo.png'
+
 const Navbar = () => {
     return (
-        <nav className="bg-gray-700 shadow-xl w-full p-4 mb-16">
-            <div className="container mx-auto">
-                <h3 className="text-green-500 text-2xl font-bold">Breaking Bad App</h3>
+        <nav className="bg-slate-400 shadow-xl w-full p-4 mb-16">
+            <div className="container mx-auto flex justify-between items-center px-24">
+                <img src={logo} className="w-36 h-14" alt="Logo" />
+                <h3 className="text-green-900 text-2xl font-bold">Breaking Bad App</h3>
             </div>
         </nav>
     )
