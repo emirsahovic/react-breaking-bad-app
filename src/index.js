0fa8d716e9b1d6d4513c0769172118656497151c
emirsahovic <emir.sahovic00@gmail.com>
2022-02-09 11:55:36 +0100

    CharacterProvider added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { CharacterProvider } from './context/CharacterContext';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CharacterProvider>
+      <App />
+    </CharacterProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
