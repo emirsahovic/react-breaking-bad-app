9e8ae897a3062d98c8f423878124a77132b95651
emirsahovic <emir.sahovic00@gmail.com>
2022-02-09 14:15:16 +0100

    Background color changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="bg-slate-600 shadow-xl w-full p-4 mb-16">
+        <nav className="bg-slate-700 shadow-xl w-full p-4 mb-16">
             <div className="container mx-auto flex justify-between items-center px-24">
                 <Link to="/"><img src={logo} className="w-32 h-20" alt="Logo" /></Link>
                 <h3 className="text-green-600 text-2xl font-bold">Breaking Bad App</h3>
