93d43f122dd9742ce37ef58e4895251b8cbf4f85
emirsahovic <emir.sahovic00@gmail.com>
2022-02-09 14:07:39 +0100

    Added link to single character page

--- src/components/CharacterItem/CharacterItem.jsx
+++ src/components/CharacterItem/CharacterItem.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const CharacterItem = ({ chars }) => {
     return (
         <div className="shadow-2xl rounded-xl p-6 mt-4 hover:scale-105 transition duration-500">
@@ -5,7 +7,7 @@ const CharacterItem = ({ chars }) => {
             <h2 className="text-gray-200 font-bold text-xl mt-2">{chars.name}</h2>
             <p className="font-semibold mt-2 text-gray-300">{chars.occupation[0]}</p>
             <p className="text-sm font-semibold text-gray-400 my-2">Portrayed: {chars.portrayed}</p>
-            <button className="py-2 px-3 my-4 bg-green-800 rounded-lg font-bold hover:bg-green-700 transition duration-500">Read More</button>
+            <Link to={`/character/${chars.char_id}`}><button className="py-2 px-3 my-4 bg-green-800 rounded-lg font-bold hover:bg-green-700 transition duration-500">Read More</button></Link>
         </div>
     )
 }
