15dd69814ee95b7110c422957802c46cc7b485c0
emirsahovic <emir.sahovic00@gmail.com>
2022-02-09 14:06:29 +0100

    React router v6 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import CharacterList from "./components/CharacterList/CharacterList";
 import Navbar from "./components/Navbar/Navbar";
 import './index.css';
+import { Routes, Route } from 'react-router-dom';
+import Character from "./pages/Character/Character";
 
 function App() {
   return (
-    <div className="h-screen">
+    <div>
       <Navbar />
-      <CharacterList />
+      <Routes>
+        <Route path="/" element={<CharacterList />} />
+        <Route path="/character/:char_id" element={<Character />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { CharacterProvider } from './context/CharacterContext';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CharacterProvider>
-      <App />
-    </CharacterProvider>
+    <Router>
+      <CharacterProvider>
+        <App />
+      </CharacterProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
