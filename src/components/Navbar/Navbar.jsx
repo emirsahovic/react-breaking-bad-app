92c2a6a6e0e6eea528788e0d143b8190d3edf3cf
emirsahovic <emir.sahovic00@gmail.com>
2022-02-09 14:04:54 +0100

    New logo added to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import logo from '../../images/logo.png'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="bg-slate-400 shadow-xl w-full p-4 mb-16">
+        <nav className="bg-slate-600 shadow-xl w-full p-4 mb-16">
             <div className="container mx-auto flex justify-between items-center px-24">
-                <img src={logo} className="w-36 h-14" alt="Logo" />
-                <h3 className="text-green-900 text-2xl font-bold">Breaking Bad App</h3>
+                <Link to="/"><img src={logo} className="w-32 h-20" alt="Logo" /></Link>
+                <h3 className="text-green-600 text-2xl font-bold">Breaking Bad App</h3>
             </div>
         </nav>
     )
